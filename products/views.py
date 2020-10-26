a2c3fa54fda16457bd8c81a89a4a4913753a0e00
ryan_tran <ryantran2165@gmail.com>
2020-10-26 16:54:54 -0700

    Delegate items per page to frontend for products view

--- products/views.py
+++ products/views.py
@@ -4,8 +4,6 @@ from .models import Product
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
-ITEMS_PER_PAGE = 20
-
 
 class ProductView(viewsets.ReadOnlyModelViewSet):
     queryset = Product.objects.all()
@@ -13,9 +11,10 @@ class ProductView(viewsets.ReadOnlyModelViewSet):
     permission_classes = [AllowAny]
 
     def list(self, request):
+        items = int(self.request.query_params.get('items', 20))
         page = int(self.request.query_params.get('page', 1))
-        start = ITEMS_PER_PAGE * (page - 1)
-        end = ITEMS_PER_PAGE * page
+        start = items * (page - 1)
+        end = items * page
         queryset = Product.objects.all()[start:end]
         serializer = ProductSerializer(queryset, many=True)
         return Response({"products": serializer.data, "count": Product.objects.count()})
