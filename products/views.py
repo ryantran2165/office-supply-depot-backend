f97bdff2614a9fa4fb6b29a09f14fd6462dce6b0
ryan_tran <ryantran2165@gmail.com>
2020-10-26 00:38:17 -0700

    Add pages to products view

--- products/views.py
+++ products/views.py
@@ -2,9 +2,20 @@ from rest_framework import viewsets
 from .serializers import ProductSerializer
 from .models import Product
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
+ITEMS_PER_PAGE = 20
 
 
 class ProductView(viewsets.ReadOnlyModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [AllowAny]
+
+    def list(self, request):
+        page = int(self.request.query_params.get('page', 1))
+        start = ITEMS_PER_PAGE * (page - 1)
+        end = ITEMS_PER_PAGE * page
+        queryset = Product.objects.all()[start:end]
+        serializer = ProductSerializer(queryset, many=True)
+        return Response({"products": serializer.data, "count": Product.objects.count()})
